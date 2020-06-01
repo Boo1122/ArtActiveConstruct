c0bffbf5adfd02bdb8e7f37beaf97efb5d70c633
Boo1122 <you@example.com>
2020-06-01 22:35:58 +0300

    resizing

--- art-active-construct/src/Footer.jsx
+++ art-active-construct/src/Footer.jsx
@@ -6,14 +6,15 @@ import Row from 'react-bootstrap/Row'
  function Footer() {
     return (
       <footer className='border-top footer'>
-          <Container>
-            <p className='copyright'>@Copyright 2020. All rights reserverd</p>
-            <ul className='contacts'>
-                <li id='style'>Email: bgafenco@gmail.com</li>
-                <li id='style'>Phone number: 0742 640 690</li>
-                <li id='style'>Adress: Cartier Grigorescu</li>
-            </ul>
-          </Container>
+            <div id='container'>
+              <p className='copyright'>@Copyright 2020. All rights reserved</p>
+              <ul className='contacts'>
+                <li>Email: bgafenco@gmail.com</li>
+                <li>Phone number: 0742640690</li>
+                <li>Adress: Cartier Grigorescu</li>
+              </ul>
+            </div>
+        
       </footer>
     )
 }
