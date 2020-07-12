d9da84c9cc96a22ee0a190a67e29eed567e467e3
Boo1122 <you@example.com>
2020-07-12 21:50:10 +0300

    footer adjustments

--- art-active-construct/src/Footer/Footer.jsx
+++ art-active-construct/src/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import './Footer.css';
 import React from 'react';
+import { FaFacebookSquare, FaInstagramSquare } from "react-icons/fa";
 
 
  export function Footer() {
@@ -11,11 +12,13 @@ import React from 'react';
             <div className='col'>
 
              <h4>Informatii</h4>
+             <hr className='line-one'/>
+                <br/>
 
               <ul className='list-unstyled'>
                 <li>Noutati</li>
                 <li> Politica de confidentialitate</li>
-                {/* <li>Something else</li> */}
+               
               </ul>
 
             </div>
@@ -26,19 +29,24 @@ import React from 'react';
               <ul className='list-unstyled'>
 
                 <h4>Art Active Construct</h4>
+                <hr className='line-two'/>
+                <br/>
 
                  <li><strong>Numar de telefon</strong>: (0742) 650 400</li>
                  {/* <li><strong>Adresa</strong>: Oradea, Romania</li> */}
-                 <li><strong>Email: </strong>artactiveconstruct@gmail.com</li>
+                 <li><strong>Email:</strong>artactiveconstruct@gmail.com</li>
               </ul>
             </div>
             <div className='col'>
 
              <h4>Ne puteti urmari si pe:</h4>
+             <hr className='line-three'/>
+             <br/>
 
-              <ul className='list-unstyled'>
-                <li>Facebook</li>
-                <li>Instagram</li>
+              <ul className='icons-list'>
+               
+                <FaFacebookSquare size={56} />
+                 <FaInstagramSquare size={56} />
                 {/* <li>Twitter</li> */}
               </ul>
             </div>
